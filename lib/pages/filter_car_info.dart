9a7cf5f2cf5d156dae6f3a9ff97d02c42fc73d8b
Joyal14 <dsilvajoyal@gmail.com>
2024-02-12 15:54:12 +0530

    code stuctured

--- lib/pages/filter_car_info.dart
+++ lib/pages/filter_car_info.dart
@@ -12,68 +12,72 @@ class FilterCarInfo extends StatefulWidget {
 }
 
 class _FilterCarInfoState extends State<FilterCarInfo> {
-  List<FilterModel> yearDataList = [
-    FilterModel(title: "0 to 1 Year"),
-    FilterModel(title: "1 to 3 Year"),
-    FilterModel(title: "3 to 5 Year"),
-    FilterModel(title: "5 to 7 Year"),
-    FilterModel(title: "7+ Year"),
-  ];
-
-  List<FilterModel> kmDrivenDataList = [
-    FilterModel(title: "Below 25000km"),
-    FilterModel(title: "25000 to 50000Km"),
-    FilterModel(title: "50000 to 75000Km"),
-    FilterModel(title: "75000 to 100000Km"),
-    FilterModel(title: "Above 100000Km+"),
-  ];
-
-  List<FilterModel> selectedOptions = [];
-  Map<String, int> categoryCounts = {
-    'Year': 0,
-    'Km Driven': 0,
-    'color': 0,
-    'brandname': 0,
-  };
-  String selectedCategory = '';
-  late List<CarColor> carColor = [];
-
-  late List<Brandname> brandName = [];
+  late List<FilterModel> yearDataList;
+  late List<FilterModel> kmDrivenDataList;
+  late List<FilterModel> selectedOptions;
+  late Map<String, int> categoryCounts;
+  late String selectedCategory;
+  late List<CarColor> carColor;
+  late List<Brandname> brandName;
+  late List<bool> selectedColorStates;
+  late List<bool> selectedbrandStates;
 
   @override
   void initState() {
     super.initState();
+    yearDataList = [
+      FilterModel(title: "0 to 1 Year"),
+      FilterModel(title: "1 to 3 Year"),
+      FilterModel(title: "3 to 5 Year"),
+      FilterModel(title: "5 to 7 Year"),
+      FilterModel(title: "7+ Year"),
+    ];
+
+    kmDrivenDataList = [
+      FilterModel(title: "Below 25000km"),
+      FilterModel(title: "25000 to 50000Km"),
+      FilterModel(title: "50000 to 75000Km"),
+      FilterModel(title: "75000 to 100000Km"),
+      FilterModel(title: "Above 100000Km+"),
+    ];
+
+    selectedOptions = [];
+    categoryCounts = {
+      'Year': 0,
+      'Km Driven': 0,
+      'color': 0,
+      'brandname': 0,
+    };
+    selectedCategory = '';
+    carColor = [];
+    brandName = [];
+    selectedColorStates = [];
+    selectedbrandStates = [];
     _fetchCarColor();
     _fetchBrandName();
   }
 
   Future<void> _fetchCarColor() async {
     try {
-      // Change the type of profiles to List<CarColor>
       List<CarColor> profiles = await ApiHelper.fetchCarColor();
       setState(() {
-        // Assign profiles to carColor
         carColor = profiles;
-        // Initialize selectedColorStates based on carColor length
         selectedColorStates = List.generate(carColor.length, (_) => false);
       });
     } catch (e) {
-      print('Error fetching car profiles: $e');
+      print('Error fetching car colors: $e');
     }
   }
 
   Future<void> _fetchBrandName() async {
     try {
-      // Change the type of profiles to List<CarColor>
       List<Brandname> profiles = await ApiHelper.fetchBrandName();
       setState(() {
-        // Assign profiles to carColor
         brandName = profiles;
-        // Initialize selectedColorStates based on carColor length
         selectedbrandStates = List.generate(brandName.length, (_) => false);
       });
     } catch (e) {
-      print('Error fetching car profiles: $e');
+      print('Error fetching brand names: $e');
     }
   }
 
@@ -83,43 +87,35 @@ class _FilterCarInfoState extends State<FilterCarInfo> {
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 60, 59, 59),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        title: Text('Filter', style: TextStyle(color: Colors.white)),
+        title: const Text('Filter', style: TextStyle(color: Colors.white)),
       ),
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // First column for filter options
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(
-                    height: 50, // Fixed height for the buttons
-                    child: buildFilterButton('Year'),
-                  ),
-                  SizedBox(
-                    height: 50, // Fixed height for the buttons
-                    child: buildFilterButton('Km Driven'),
-                  ),
-                  SizedBox(
-                    height: 50, // Fixed height for the buttons
-                    child: buildFilterButton('color'),
-                  ),
-                  SizedBox(
-                    height: 50, // Fixed height for the buttons
-                    child: buildFilterButton('brandname'),
-                  ),
+                  for (var category in [
+                    'Year',
+                    'Km Driven',
+                    'color',
+                    'brandname'
+                  ])
+                    SizedBox(
+                      height: 50,
+                      child: buildFilterButton(category),
+                    ),
                 ],
               ),
             ),
-            // Second column for selected filter options
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
@@ -137,32 +133,17 @@ class _FilterCarInfoState extends State<FilterCarInfo> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  clearAllSelections();
-                });
-              },
+              onPressed: clearAllSelections,
               style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-              child: const Text(
-                'Clear All',
-                style: TextStyle(color: Colors.white),
-              ),
+              child: const Text('Clear All',
+                  style: TextStyle(color: Colors.white)),
             ),
             ElevatedButton(
               onPressed: () {
-                // Navigate to FilterOptionsPage and pass the selected options
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(
-                //     builder: (context) => FilterOptionsPage(selectedOptions: selectedOptions),
-                //   ),
-                // );
+                // Apply filter logic here
               },
               style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-              child: Text(
-                'Apply',
-                style: TextStyle(color: Colors.white),
-              ),
+              child: const Text('Apply', style: TextStyle(color: Colors.white)),
             ),
           ],
         ),
@@ -173,10 +154,9 @@ class _FilterCarInfoState extends State<FilterCarInfo> {
   Widget buildFilterButton(String category) {
     int count = categoryCounts[category] ?? 0;
     return Padding(
-      padding: EdgeInsets.symmetric(
-          vertical: 3.0, horizontal: 6.0), // Adjust padding as needed
-      child: Container(
-        width: 200, // Set a fixed width for consistent button lengths
+      padding: const EdgeInsets.symmetric(vertical: 3.0, horizontal: 6.0),
+      child: SizedBox(
+        width: 200,
         child: TextButton(
           onPressed: () {
             setState(() {
@@ -194,8 +174,7 @@ class _FilterCarInfoState extends State<FilterCarInfo> {
             });
           },
           style: TextButton.styleFrom(
-            backgroundColor:
-                Color.fromARGB(255, 48, 48, 48), // Set gray background color
+            backgroundColor: const Color.fromARGB(255, 48, 48, 48),
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
           ),
@@ -204,23 +183,20 @@ class _FilterCarInfoState extends State<FilterCarInfo> {
             children: [
               Text(
                 category,
-                style:
-                    TextStyle(color: Colors.white), // Set text color to black
+                style: const TextStyle(color: Colors.white),
               ),
               if (count > 0)
                 Container(
-                  width: 24, // Set the width of the container
-                  height: 24, // Set the height of the container
-                  alignment: Alignment.center, // Center the count
+                  width: 24,
+                  height: 24,
+                  alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors
-                        .blue, // Set the background color of the container
-                    borderRadius: BorderRadius.circular(
-                        4), // Apply border radius for a rounded appearance
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(4),
                   ),
                   child: Text(
                     '${categoryCounts[category]}',
-                    style: TextStyle(color: Colors.white),
+                    style: const TextStyle(color: Colors.white),
                   ),
                 ),
             ],
@@ -230,16 +206,14 @@ class _FilterCarInfoState extends State<FilterCarInfo> {
     );
   }
 
-  List<bool> selectedColorStates = [];
-  List<bool> selectedbrandStates = [];
-
   List<Widget> buildCheckboxList() {
     List<Widget> checkboxes = [];
+
     checkboxes.addAll(selectedOptions.map((option) {
       return CheckboxListTile(
         title: Text(
           option.title,
-          style: TextStyle(color: Colors.white),
+          style: const TextStyle(color: Colors.white),
         ),
         value: option.isSelected,
         onChanged: (value) {
@@ -252,11 +226,9 @@ class _FilterCarInfoState extends State<FilterCarInfo> {
         activeColor: Colors.blue,
         checkColor: Colors.white,
       );
-    }).toList());
+    }));
 
-    // Check if the "Color" button is selected
     if (selectedCategory == 'color') {
-      // Add color checkboxes
       checkboxes.addAll(carColor.map((color) {
         int index = carColor.indexOf(color);
         return CheckboxListTile(
@@ -270,24 +242,23 @@ class _FilterCarInfoState extends State<FilterCarInfo> {
           onChanged: (value) {
             setState(() {
               selectedColorStates[index] = value!;
-              updateCounts(); // Update counts when checkbox state changes
+              updateCounts();
             });
           },
           controlAffinity: ListTileControlAffinity.leading,
           activeColor: Colors.blue,
           checkColor: Colors.white,
         );
-      }).toList());
+      }));
     }
 
     if (selectedCategory == 'brandname') {
-      // Add brand name checkboxes
-      checkboxes.addAll(brandName.map((brandname) {
-        int index = brandName.indexOf(brandname);
+      checkboxes.addAll(brandName.asMap().entries.map((entry) {
+        int index = entry.key;
+        Brandname brandname = entry.value;
         return CheckboxListTile(
           title: Text(
-            brandname.brandName ??
-                '', // Access the color property of the Doc object
+            brandname.brandName ?? '',
             style: const TextStyle(color: Colors.white),
           ),
           value: selectedbrandStates.length > index
@@ -296,14 +267,14 @@ class _FilterCarInfoState extends State<FilterCarInfo> {
           onChanged: (value) {
             setState(() {
               selectedbrandStates[index] = value!;
-              updateCounts(); // Update counts when checkbox state changes
+              updateCounts();
             });
           },
           controlAffinity: ListTileControlAffinity.leading,
           activeColor: Colors.blue,
           checkColor: Colors.white,
         );
-      }).toList());
+      }));
     }
 
     return checkboxes;
@@ -328,10 +299,8 @@ class _FilterCarInfoState extends State<FilterCarInfo> {
       for (var option in kmDrivenDataList) {
         option.isSelected = false;
       }
-
       selectedColorStates = List.generate(carColor.length, (_) => false);
       selectedbrandStates = List.generate(brandName.length, (_) => false);
-
       updateCounts();
     });
   }
